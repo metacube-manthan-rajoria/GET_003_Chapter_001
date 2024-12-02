eaf1add46f46878e159d1784da767be35cbc6b15
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-02 14:38:08 +0530

    password field now change color and number input now works

--- script.js
+++ script.js
@@ -18,13 +18,18 @@ function hideSection(elements){
 
 function nextEmployeeSection(){
     let elements = document.getElementsByClassName("employee_form_item");
+    let formMessage = document.getElementById("employee_form_message");
     
-    if(currentItem1.itemIndex < elements.length - 1){
+    if(currentItem1.itemIndex < elements.length){
         if(currentItem1.itemIndex == 0){
             let textBox = document.getElementById("employee-name");
             let textBoxValue = textBox.value;
 
-            if(textBoxValue.length < 2 || hasNumber.test(textBoxValue)) return;
+            if(textBoxValue.length < 2 || hasNumber.test(textBoxValue)){
+                formMessage.style.display = "block";
+                formMessage.innerText = "Enter valid name";
+                return;
+            } 
             currentItem1.employeeName = textBoxValue;
             let label = document.getElementById("employee_form_gender_label");
             label.innerText = "Hi " + currentItem1.employeeName + ", Can I know your gender.";
@@ -33,15 +38,46 @@ function nextEmployeeSection(){
             let radio2 = document.getElementById("employee-gender-female");
             let radio3 = document.getElementById("employee-gender-other");
 
-            if(!radio1.checked && !radio2.checked && !radio3.checked) return;
+            if(!radio1.checked && !radio2.checked && !radio3.checked){
+                formMessage.style.display = "block";
+                formMessage.innerText = "Select one of the options";
+                return;
+            };
             let label = document.getElementById("employee_form_email_label");
             label.innerText = "Hi " + currentItem1.employeeName + ", Can I know your Email.";
         }else if(currentItem1.itemIndex == 2){
             let textBox = document.getElementById("employee-email");
             let textBoxValue = textBox.value;
 
-            if(!textBox.validity.valid) return;
+            if(!textBox.validity.valid){
+                formMessage.style.display = "block";
+                formMessage.innerText = "Enter valid email";
+                return;
+            };
             currentItem1.employeeEmail = textBoxValue;
+
+            document.getElementById("employee-password").addEventListener("blur", (e)=>{
+                if(e.target.value.length < 8){
+                    e.target.style.border = "2px solid red";
+                }else if(e.target.value.length < 10){
+                    e.target.style.border = "2px solid orange";
+                }else if(e.target.value.length < 12){
+                    e.target.style.border = "2px solid yellow";
+                }else{
+                    e.target.style.border = "2px solid green";
+                }
+            });
+            document.getElementById("employee-password-confirmation").addEventListener("focusout", (e)=>{
+                if(e.target.value.length < 8){
+                    e.target.style.border = "2px solid red";
+                }else if(e.target.value.length < 10){
+                    e.target.style.border = "2px solid orange";
+                }else if(e.target.value.length < 12){
+                    e.target.style.border = "2px solid yellow";
+                }else{
+                    e.target.style.border = "2px solid green";
+                }
+            });
         }else if(currentItem1.itemIndex == 3){
             let textBox1 = document.getElementById("employee-password");
             let textBox2 = document.getElementById("employee-password-confirmation");
@@ -57,13 +93,27 @@ function nextEmployeeSection(){
             let textBox = document.getElementById("employee-phone-number");
             let textBoxValue = textBox.value;
 
-            if(!textBox.validity.valid) return;
+            if(textBoxValue.length < 6){
+                formMessage.style.display = "block";
+                formMessage.innerText = "Enter valid number";
+                return;
+            };
             currentItem1.employeeNumber = textBoxValue;
+
+            let employeeSection = document.getElementById("employee_section");
+            employeeSection.style.display = "none";
+            let vehicleSection = document.getElementById("vehicle_section");
+            vehicleSection.style.display = "block";
+
+            return;
         }
 
         currentItem1.itemIndex++;
+        console.log(currentItem1.itemIndex);
         hideSection(elements);
         elements[currentItem1.itemIndex].style.display = "block";
+        formMessage.innerText = "";
+        formMessage.style.display = "none";
     }else{
         elements[elements.length - 1].style.display = "block";
     }
